54f6ee97695f7f57ca43b331e3f48d75eeb57dff
Marcus Allen <marcus.allen84@thehutgroup.com>
2024-10-04 21:03:57 +0100

    added some more questions

--- src/api/questions.ts
+++ src/api/questions.ts
@@ -10,4 +10,29 @@ const questionLink1: QuestionLink = {
   link: ["MANAGER", "ENGLAND", "FOOTBALL"],
 };
 
-export const questionBank = [questionLink1];
+const questionLink2: QuestionLink = {
+  question1:
+    "Which character did Danny DeVito play in the 1992 film Batman Returns",
+  question2:
+    "James “Clubber” Lang, Tommy Gunn and Ivan Drago are all opponents of which fictional character",
+  question3:
+    "French Riviera on the south coast of France, this is France’s fifth most populous city.",
+  answer1: "penguin",
+  answer2: "rocky",
+  answer3: "nice",
+  link: ["biscuit"],
+};
+
+const questionLink3: QuestionLink = {
+  question1:
+    "Melbourne is the capital and most populous city of this Australian state",
+  question2:
+    "Which comic book character has been portrayed by actors Jeremy Irons and Michael Caine?",
+  question3:
+    "This American singer-songwriter, commonly nicknamed the Piano Man had a UK number one single in 1983 with “Uptown Girl”",
+  answer1: "victoria",
+  answer2: "alfred",
+  answer3: "billy joel",
+  link: ["monarchs", "british", "kings", "queens"],
+};
+export const questionBank = [questionLink1, questionLink2, questionLink3];
